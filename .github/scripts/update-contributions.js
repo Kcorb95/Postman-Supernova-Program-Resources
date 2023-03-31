545b0e9d6841fa2cceb5d8066b57ecb9d7f70344
Kevin <40337763+Kcorb95@users.noreply.github.com>
2023-03-31 10:27:17 -0400

    chore: Re-wrote contribution logic. Should be working now...

--- .github/scripts/update-contributions.js
+++ .github/scripts/update-contributions.js
@@ -10,86 +10,112 @@ async function run() {
     const contributionUrl = payload.inputs.contribution_url;
     const contributionAuthor = payload.inputs.contribution_author;
     let contributionDate = new Date(payload.inputs.contribution_date);
-    
+
     // Log Inputs
     console.log(`Contribution Name: ${contributionName}`);
     console.log(`Contribution URL: ${contributionUrl}`);
     console.log(`Contribution Author: ${contributionAuthor}`);
     console.log(`Contribution Date: ${contributionDate}`);
-    
+
     // Format the date as "MMM. Dth"
     const nth = (d) => {
-      if (d > 3 && d < 21) return 'th';
+      if (d > 3 && d < 21) return "th";
       switch (d % 10) {
-        case 1:  return "st";
-        case 2:  return "nd";
-        case 3:  return "rd";
-        default: return "th";
+        case 1:
+          return "st";
+        case 2:
+          return "nd";
+        case 3:
+          return "rd";
+        default:
+          return "th";
       }
-    }
-    contributionDate = contributionDate.toLocaleString('default', { month: 'short' }) + '. ' + contributionDate.getDate() + nth(contributionDate.getDate());
+    };
 
-    // Get the content of the file
-    const filePath = "pages/contributions/Current-Contributions.md";
-    const fileContent = await fs.readFile(filePath, 'utf8');
-    console.log(`Old File Content Length: ${fileContent.length}`);
+    // Format inputted date correctly (MMM. Dth)
+    const formattedDate = contributionDate.toLocaleString("default", { month: "short" }) + ". " + contributionDate.getDate() + nth(contributionDate.getDate());
 
-// Find the current year's header and its index
-const currentYear = new Date().getFullYear();
-const currentYearHeader = `## ${currentYear}`;
-const currentYearHeaderIndex = fileContent.indexOf(currentYearHeader);
+    // Format our new entry correctly
+    const newEntry = `- ${formattedDate}: [${contributionName}](${contributionUrl}) by ${contributionAuthor}`;
+    console.log("New Entry" + newEntry + "\n");
 
-// Find the last entry in the current year
-let lastEntryIndex = fileContent.indexOf(currentYearHeader, currentYearHeaderIndex + 1);
-if (lastEntryIndex === -1) {
-  lastEntryIndex = fileContent.length;
-}
+    // Read in file
+    const filePath = "./Contributions.md";
+    const fileContent = await fs.readFile(filePath, "utf8");
 
-// Build the new entry
-const newEntry = `- ${contributionDate}: [${contributionName}](${contributionUrl}) by ${contributionAuthor}\n`;
+    // Find current year's header
+    const yearHeaderIndex = fileContent.indexOf(`## ${contributionDate.getFullYear()}`);
+    if (yearHeaderIndex === -1) return console.log("Year not found.");
 
-// Split the file content into an array of lines
-const lines = fileContent.split("\n");
+    // Get index of previous year's header
+    const lastYearIndex = fileContent.indexOf(`## ${contributionDate.getFullYear() - 1}`);
+    // TODO: ^^ This errors if there is no previous year.
 
-// Find the correct location to insert the new entry
-let insertIndex = lastEntryIndex;
-for (let i = currentYearHeaderIndex + 1; i < lastEntryIndex; i++) {
-  const line = lines[i];
-  const monthMatch = line.match(/^(\w{3})$/);
-  if (monthMatch) {
-    const month = monthMatch[1];
-    const nextLine = lines[i + 1];
-    if (nextLine) {
-      const dateMatch = nextLine.match(/^-\s(\d{1,2})/);
-      if (dateMatch) {
-        const day = parseInt(dateMatch[1]);
-        const entryDate = new Date(`${month} ${day}, ${currentYear}`);
-        if (entryDate > contributionDate) {
-          insertIndex = i + 1;
-          break;
+    // Split text to then parse through for correct month
+    const firstNewLineIndex = fileContent.indexOf("\n", yearHeaderIndex);
+    const lastNewLineIndex = fileContent.lastIndexOf("\n", lastYearIndex);
+
+    const contributions = fileContent
+      .substring(firstNewLineIndex + 1, lastNewLineIndex)
+      .split("\n");
+
+    // If empty, insert at top
+    if (contributions.length === 1) {
+      console.log(`No contributions this month, pushing...`);
+      contributions.splice(1, 0, newEntry);
+    } else {
+      console.log("Searching for correct position to insert new data...")
+      // Find correct place to insert
+      let found = false;
+      let index = -1;
+
+      while (!found) {
+        index++;
+        const contribution = contributions[index];
+
+        if (contribution.length === 0) return; // New line/null situations
+
+        // Get DATE part of string -- "- Mar. 24th: [EVENT NAME](URL) by AUTHOR"
+        const dateStr = contribution.substring(2, contribution.indexOf(":"));
+        // We can infer the year due to how we are inserting this data.
+        // Appending year is important for the proper date calculation.
+        const date = Date.parse(`${dateStr.slice(0, -2)}, ${contributionDate.getFullYear()}`);
+
+        // Insert at first instance of date being older than new entry
+        if (date > contributionDate) {
+          console.log(`Found index in current month ${index}, pushing...`);
+          found = true;
+          contributions.splice(index, 0, newEntry);
+        } else if (index === contributions.length - 2) {
+          // Or at end of array as it would be the oldest item
+          console.log(`Oldest date, adding to end of list...`);
+          found = true;
+          contributions.splice(index + 1, 0, newEntry);
         }
       }
     }
-  }
-}
+    // Write our contributions to file in correct location.
+    const newFileContents = fileContent.substring(0, firstNewLineIndex) + "\n" + contributions.join("\n") + fileContent.substring(lastNewLineIndex);
 
-// Insert the new entry in the correct place
-lines.splice(insertIndex, 0, newEntry);
-const newContent = lines.join("\n");
-    
+    console.log("------");
+    console.log("New Contents:");
+    console.log(newFileContents);
+    console.log("Contributions Data:");
+    console.log(contributions);
+
     // Commit the changes
     const accessToken = process.env.PERSONAL_ACCESS_TOKEN;
     const octokit = github.getOctokit(accessToken);
     console.log(`Access Token: ${accessToken}`);
     console.log(octokit.repos);
-    
+
     const { data: { sha: currentSha } } = await octokit.rest.repos.getContent({
       owner: "Kcorb95",
       repo: "Postman-Supernova-Program-Resources",
       path: filePath,
       ref: "main"
     });
-    
+
     const commit = await octokit.rest.repos.createOrUpdateFileContents({
       owner: "Kcorb95",
       repo: "Postman-Supernova-Program-Resources",
@@ -99,7 +125,7 @@ const newContent = lines.join("\n");
         name: "Kevin Corbett",
         email: "kevin.corbett08@gmail.com"
       },
-      content: Buffer.from(newContent).toString('base64'),
+      content: Buffer.from(newFileContents).toString('base64'),
       branch: 'main',
       sha: currentSha,
     });
